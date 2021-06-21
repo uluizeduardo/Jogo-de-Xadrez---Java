d2535f4e7d8748a28aa984d0e8298e503ba6b143
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-06-21 19:49:59 -0300

    Pequena melhoria na impressão do tabuleiro

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -34,6 +34,18 @@ public class PartidaDeXadrez {//Essa Classe é responsável pelas regras do jogo
     private void inicioDoJogo(){
         colocarNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
-        colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+        colocarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
     }
 }
